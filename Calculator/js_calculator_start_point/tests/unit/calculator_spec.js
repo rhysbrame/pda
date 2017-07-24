e39e1a2bf5e2d87dae8005738d8546302774a1a9
rhysbrame <rhysbrame@gmail.com>
2017-07-24 13:20:24 +0100

    created unit tests

--- Calculator/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Calculator/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -6,9 +6,72 @@ describe('calculator', function () {
     calculator = new Calculator()
   });
 
-  // write unit tests here in the form of "it should do something..."
-  it('it has a sample test', function(){
-    assert.equal(true, true)
+  it('is possible to add numbers', function(){
+   calculator.add(10)
+   assert.equal(calculator.runningTotal, 10)
+ })
+
+  it('is possible to subtract', function(){
+    calculator.previousTotal = 10
+    calculator.subtract(10)
+    assert.equal(calculator.runningTotal, 0)
   })
+
+  it('is possible to multiply', function(){
+    calculator.previousTotal = 10
+    calculator.multiply(2)
+    assert.equal(calculator.runningTotal, 20)
+  })
+
+  it('is possible to divide', function(){
+    calculator.previousTotal = 10
+    calculator.divide(2)
+    assert.equal(calculator.runningTotal, 5)
+  })
+
+  it('is possible to divide two numbers to give a float', function(){
+   calculator.previousTotal = 11
+   calculator.divide(2)
+   assert.equal(calculator.runningTotal, 5.5)
+ })
+
+  it('is possible to add with operatorClick', function(){
+   calculator.numberClick(3)
+   calculator.operatorClick('*')
+   calculator.numberClick(5)
+   calculator.operatorClick('=')
+   assert.equal(calculator.runningTotal, 15)
+ })
+
+  it('is possible to multiply with numberClick and operatorClick from runningTotal', function(){
+   calculator.numberClick(5)
+   calculator.numberClick(0)
+   calculator.operatorClick('*')
+   calculator.numberClick(2)
+   calculator.operatorClick('=')
+   assert.equal(calculator.runningTotal, 100)
+ })
+
+  it('is possible to use multiple operations in one function', function(){
+   calculator.numberClick(14)
+   calculator.operatorClick('+')
+   calculator.numberClick(1)
+   calculator.operatorClick('*')
+   calculator.numberClick(2)
+   calculator.operatorClick('-')
+   calculator.numberClick(10)
+   calculator.operatorClick('/')
+   calculator.numberClick(2)
+   calculator.operatorClick('=')
+   assert.equal(calculator.runningTotal, 10)
+  })
+
+  it('is possible to reset runningTotal', function(){
+   calculator.numberClick(42)
+   calculator.operatorClick('+')
+   calculator.numberClick(91)
+   calculator.clearClick()
+   assert.equal(calculator.runningTotal, 0)
+ })
 
 });
